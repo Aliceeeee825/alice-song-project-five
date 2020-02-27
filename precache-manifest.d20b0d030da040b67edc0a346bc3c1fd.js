2db16985bc40ee602765821ce54ec175dd3f3c41
Aliceeeee825 <zhuyingsong0825@gmail.com>
2020-02-27 12:38:47 -0500

    Updates

---
+++ precache-manifest.d20b0d030da040b67edc0a346bc3c1fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e76505d43fa227640218db7e2eee57d",
+    "url": "/alice-song-project-five/index.html"
+  },
+  {
+    "revision": "fe10d1bc1653e848a105",
+    "url": "/alice-song-project-five/static/css/main.3c282221.chunk.css"
+  },
+  {
+    "revision": "96d1f64bad408155525c",
+    "url": "/alice-song-project-five/static/js/2.e2c018ba.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/alice-song-project-five/static/js/2.e2c018ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe10d1bc1653e848a105",
+    "url": "/alice-song-project-five/static/js/main.a79787aa.chunk.js"
+  },
+  {
+    "revision": "b562a1abafc37ff297e0",
+    "url": "/alice-song-project-five/static/js/runtime-main.f6fd186f.js"
+  }
+]);
