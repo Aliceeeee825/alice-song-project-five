7149629f6073b6a64a577ba68d3048a20d0e1fc7
Aliceeeee825 <zhuyingsong0825@gmail.com>
2020-03-24 16:40:50 -0400

    Updates

--- precache-manifest.473dde5750bd6908d899fef8785b47dc.js
+++ precache-manifest.473dde5750bd6908d899fef8785b47dc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "66bc23176e7898ae6878baa972a8f34b",
+    "revision": "14e68df302ce69e0a3268d63e6d97fd1",
     "url": "/alice-song-project-five/index.html"
   },
   {
-    "revision": "c407d14e67d4bf4589c9",
+    "revision": "4d18d458c5a2b4cbc699",
     "url": "/alice-song-project-five/static/css/main.136f3a8a.chunk.css"
   },
   {
-    "revision": "4be5448b1a92c1862b12",
-    "url": "/alice-song-project-five/static/js/2.a8266835.chunk.js"
+    "revision": "26b649f273b65667f3ae",
+    "url": "/alice-song-project-five/static/js/2.038b309f.chunk.js"
   },
   {
     "revision": "4691121c96f91c7f8618b1ecc9480cfc",
-    "url": "/alice-song-project-five/static/js/2.a8266835.chunk.js.LICENSE.txt"
+    "url": "/alice-song-project-five/static/js/2.038b309f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c407d14e67d4bf4589c9",
-    "url": "/alice-song-project-five/static/js/main.6093de79.chunk.js"
+    "revision": "4d18d458c5a2b4cbc699",
+    "url": "/alice-song-project-five/static/js/main.3007096b.chunk.js"
   },
   {
     "revision": "b562a1abafc37ff297e0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/alice-song-project-five/precache-manifest.b266f4dfc28f3f0a37e54c80e4eb5f68.js"
+  "/alice-song-project-five/precache-manifest.473dde5750bd6908d899fef8785b47dc.js"
 );
 
 self.addEventListener('message', (event) => {
